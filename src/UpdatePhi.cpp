72770d4dc6d99ca253956b7357cbea4de3f2ca2c
ndmarco96@g.ucla.edu <ndmarco96@g.ucla.edu>
2021-05-06 11:24:29 -0700

    Updated the notes to reflect stationarity guarantees of mixing Tempered Transitions MCMC and M-H

--- src/UpdatePhi.cpp
+++ src/UpdatePhi.cpp
@@ -120,3 +120,124 @@ void updatePhi(const arma::field<arma::vec>& y_obs,
     Phi(iter + 1,0) = Phi(iter,0);
   }
 }
+
+//' Updates the Phi parameters using a Tempered Transition
+//'
+//' @name UpdatePhiTempered
+//' @param beta_i Double containing the current temperature
+//' @param y_obs Field of Vectors containing observed time points
+//' @param y_star Field of Matrices contianing unobserved time points at all mcmc iterations
+//' @param B_obs Field of Matrices containing basis functions evaluated at observed time points
+//' @param B_star Field of Matrices containing basis functions evaluated at unobserved time points
+//' @param nu Matrix containing current nu parameters
+//' @param gamma Cube containing current gamma parameters
+//' @param tilde_tau vector containing current tilde_tau parameters
+//' @param Z Matrix containing current Z parameters
+//' @param sigma_sq double containing the sigma_sq variable
+//' @param chi Matrix containing chi values
+//' @param iter int containing current mcmc sample
+//' @param m_1 Vector acting as a placeholder for m in mean vector
+//' @param M_1 Matrix acting as a placeholder for M in covariance
+//' @param Phi Field of Cubes containing all mcmc samples of Phi
+
+void updatePhiTempered(const double& beta_i,
+                       const arma::field<arma::vec>& y_obs,
+                       const arma::field<arma::mat>& y_star,
+                       const arma::field<arma::mat>& B_obs,
+                       const arma::field<arma::mat>& B_star,
+                       const arma::mat& nu,
+                       const arma::cube& gamma,
+                       const arma::vec& tilde_tau,
+                       const arma::mat& Z,
+                       const arma::mat& chi,
+                       const double& sigma_sq,
+                       const int& iter,
+                       const int& tot_mcmc_iters,
+                       arma::vec& m_1,
+                       arma::mat& M_1,
+                       arma::field<arma::cube>& Phi){
+  m_1.zeros();
+  M_1.zeros();
+  double ph = 0;
+
+  for(int j =  0; j < Phi(iter,0).n_rows; j ++){
+    for(int m = 0; m < Phi(iter,0).n_slices; m++){
+      m_1.zeros();
+      M_1.zeros();
+      for(int i = 0; i < Z.n_rows; i++){
+        if(Z(i,j) != 0){
+          for(int l = 0; l < y_obs(i,0).n_elem; l++){
+            ph = 0;
+            ph = y_obs(i,0)(l) - arma::dot(nu.row(j),
+                       B_obs(i,0).row(l));
+            M_1 = M_1 + (chi(i,m) * chi(i,m) * B_obs(i,0).row(l).t() *
+              B_obs(i,0).row(l));
+            for(int k = 0; k < nu.n_rows; k++){
+              for(int n = 0; n < Phi(iter,0).n_slices; n++){
+                if(k == j){
+                  if(n != m){
+                    ph = ph - (chi(i,n) *
+                      arma::dot(Phi(iter,0).slice(n).row(k), B_obs(i,0).row(l)));
+                  }
+                }else{
+                  ph = ph - (Z(i,k) * chi(i,n) *
+                    arma::dot(Phi(iter,0).slice(n).row(k),  B_obs(i,0).row(l)));
+                }
+              }
+              if(k != j){
+                ph = ph - Z(i,k) * arma::dot(nu.row(k), B_obs(i,0).row(l));
+              }
+            }
+            m_1 = m_1 +  chi(i,m) * B_obs(i,0).row(l).t() * ph;
+          }
+
+
+          // Check to see if there are unobserved time points of interest
+          if(B_star(i,0).n_elem > 0){
+            for(int l = 0; l < y_star(i,0).n_cols; l++){
+              ph = 0;
+              ph = y_star(i,0)(iter,l) - arma::dot(nu.row(j),
+                          B_star(i,0).row(l));
+              M_1 = M_1 +  (chi(i,m) * chi(i,m) * B_star(i,0).row(l).t() *
+                B_star(i,0).row(l));
+              for(int k = 0; k < nu.n_rows; k++){
+                for(int n = 0; n < Phi(iter,0).n_slices; n++){
+                  if(k == j){
+                    if(n != m){
+                      ph = ph - (chi(i,n) *
+                        arma::dot(Phi(iter,0).slice(n).row(k),
+                                  B_star(i,0).row(l)));
+                    }
+                  }else{
+                    ph = ph - (Z(i,k) * chi(i,n) *
+                      arma::dot(Phi(iter,0).slice(n).row(k),
+                                B_star(i,0).row(l)));
+                  }
+                }
+                if(k != j){
+                  ph = ph - Z(i,k) * arma::dot(nu.row(k), B_star(i,0).row(l));
+                }
+              }
+              m_1 = m_1 +  chi(i,m) * B_star(i,0).row(l).t() * ph;
+            }
+          }
+        }
+      }
+      m_1 = m_1 * (beta_i / sigma_sq);
+      M_1 = M_1 * (beta_i / sigma_sq);
+
+      //Add on diagonal component
+      for(int k = 0; k < M_1.n_rows; k++){
+        M_1(k,k) = M_1(k,k) + tilde_tau(m) * gamma.slice(m)(j,k);
+      }
+      arma::inv(M_1, M_1);
+
+      //generate new sample
+      Phi(iter,0).slice(m).row(j) =  arma::mvnrnd(M_1 * m_1, M_1).t();
+    }
+  }
+  // Update next iteration
+  if(iter < (tot_mcmc_iters - 1)){
+    Phi(iter + 1,0) = Phi(iter,0);
+  }
+}
--- src/UpdateSigma.cpp
+++ src/UpdateSigma.cpp
@@ -17,6 +17,7 @@
 //' @param iter Int containing current MCMC iteration
 //' @param tot_mcmc_iters Int containing total number of MCMC iterations
 //' @param sigma Vector containing sigma for all mcmc iterations
+
 void updateSigma(const arma::field<arma::vec>& y_obs,
                  const arma::field<arma::mat>& y_star,
                  const arma::field<arma::mat>& B_obs,
@@ -62,6 +63,81 @@ void updateSigma(const arma::field<arma::vec>& y_obs,
         b_1 = b_1 + 0.5 * (b * b);
       }
       a = a + (y_star(i,0).n_cols / 2);
+    }
+  }
+  b_1 = b_1 + beta_0;
+  a = a + alpha_0;
+  sigma(iter) = 1 / R::rgamma(a, 1/b_1);
+
+  if(iter < (tot_mcmc_iters - 1)){
+    sigma(iter + 1) = sigma(iter);
+  }
+}
+
+//' Updates the Sigma parameters using Tempered Transitions
+//'
+//' @name updateSigma
+//' @param beta_i Double containing current temperature
+//' @param y_obs Field of vectors containing observed time points
+//' @param y_obs Field of matrices containing unobserved time points for all iterations
+//' @param B_obs Field of matrices containing basis functions evaluated at observed time points
+//' @param B_star Field of matrices containing basis functions evaluated at unobserved time points
+//' @param alpha_0 Double containing hyperparameter
+//' @param beta_0 Double containing hyperparameter
+//' @param nu Matrix contianing current nu parameters
+//' @param Phi Cube containing current Phi parameters
+//' @param Z Matrix containing current Z parameters
+//' @param chi Matrix containing current chi parameters
+//' @param iter Int containing current MCMC iteration
+//' @param tot_mcmc_iters Int containing total number of MCMC iterations
+//' @param sigma Vector containing sigma for all mcmc iterations
+
+void updateSigmaTempered(const double& beta_i,
+                         const arma::field<arma::vec>& y_obs,
+                         const arma::field<arma::mat>& y_star,
+                         const arma::field<arma::mat>& B_obs,
+                         const arma::field<arma::mat>& B_star,
+                         const double alpha_0,
+                         const double beta_0,
+                         const arma::mat& nu,
+                         const arma::cube& Phi,
+                         const arma::mat& Z,
+                         const arma::mat& chi,
+                         const int& iter,
+                         const int& tot_mcmc_iters,
+                         arma::vec& sigma){
+  double a = 0;
+  double b = 0;
+  double b_1 = 0;
+  for(int i = 0; i < Z.n_rows; i++){
+    for(int l = 0; l < y_obs(i,0).n_elem; l++){
+      b = y_obs(i,0)(l);
+      for(int k = 0; k < Z.n_cols; k++){
+        if(Z(i,k) != 0){
+          b = b - arma::dot(nu.row(k), B_obs(i,0).row(l));
+          for(int n = 0; n < Phi.n_slices; n++){
+            b = b - chi(i,n) * arma::dot(Phi.slice(n).row(k), B_obs(i,0).row(l));
+          }
+        }
+      }
+      b_1 = b_1 + (beta_i / 2) * (b * b);
+    }
+    a = a + ((beta_i * y_obs(i,0).n_elem) / 2);
+    if(B_star(i,0).n_elem > 0){
+      for(int l = 0; l < y_star(i,0).n_cols; l++){
+        b = y_star(i,0)(iter, l);
+        for(int k = 0; k < Z.n_cols; k++){
+          if(Z(i,k) != 0){
+            b = b - arma::dot(nu.row(k), B_star(i,0).row(l));
+            for(int n = 0; n < Phi.n_slices; n++){
+              b = b - chi(i,n) * arma::dot(Phi.slice(n).row(k),
+                          B_star(i,0).row(l));
+            }
+          }
+        }
+        b_1 = b_1 + (beta_i / 2) * (b * b);
+      }
+      a = a + ((beta_i * y_star(i,0).n_cols) / 2);
     }
   }
   b_1 = b_1 + beta_0;
