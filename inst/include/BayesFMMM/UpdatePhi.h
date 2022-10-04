c731c10232d3cb8ffda811b912687b29812de12b
ndmarco <ndmarco96@g.ucla.edu>
2022-10-04 16:04:32 -0700

    Finished posterior sampling for phi parameters for covariate adjusted model

--- inst/include/BayesFMMM/UpdatePhi.h
+++ inst/include/BayesFMMM/UpdatePhi.h
@@ -393,7 +393,7 @@ inline void updatePhiCovariateAdj(const arma::field<arma::vec>& y_obs,
                 }else{
                   ph = ph - (Z(i,k) * chi(i,n) *
                     arma::dot((Phi(iter,0).slice(n).row(k) +
-                    xi(iter,k).slice(n) * X.row(i).t()),  B_obs(i,0).row(l)));
+                    (xi(iter,k).slice(n) * X.row(i).t()).t()),  B_obs(i,0).row(l)));
                 }
               }
               if(k != j){
@@ -463,7 +463,7 @@ inline void updatePhiTemperedCovariateAdj(const double& beta_i,
   m_1.zeros();
   M_1.zeros();
   double ph = 0;
-  
+
   for(int j =  0; j < Phi(iter,0).n_rows; j ++){
     for(int m = 0; m < Phi(iter,0).n_slices; m++){
       m_1.zeros();
@@ -489,7 +489,7 @@ inline void updatePhiTemperedCovariateAdj(const double& beta_i,
                 }else{
                   ph = ph - (Z(i,k) * chi(i,n) *
                     arma::dot((Phi(iter,0).slice(n).row(k) +
-                    xi(iter,k).slice(n) * X.row(i).t()),  B_obs(i,0).row(l)));
+                    (xi(iter,k).slice(n) * X.row(i).t()).t()),  B_obs(i,0).row(l)));
                 }
               }
               if(k != j){
@@ -503,13 +503,183 @@ inline void updatePhiTemperedCovariateAdj(const double& beta_i,
       }
       m_1 = m_1 * (beta_i / sigma_sq);
       M_1 = M_1 * (beta_i / sigma_sq);
-      
+
       //Add on diagonal component
       for(int k = 0; k < M_1.n_rows; k++){
-        M_1(k,k) = M_1(k,k) + tilde_tau(j,m) * gamma.slice(m)(j,k);
+        M_1(k,k) = M_1(k,k) + tilde_tau_phi(j,m) * gamma.slice(m)(j,k);
       }
       arma::inv(M_1, M_1);
-      
+
+      //generate new sample
+      Phi(iter,0).slice(m).row(j) =  arma::mvnrnd(M_1 * m_1, M_1).t();
+    }
+  }
+  // Update next iteration
+  if(iter < (tot_mcmc_iters - 1)){
+    Phi(iter + 1,0) = Phi(iter,0);
+  }
+}
+
+// Updates the Phi parameters for the covariate adjusted multivariate model
+//
+// @name UpdatePhiMVCovariateAdj
+// @param y_obs Matrix containing observed vectors
+// @param nu Matrix containing current nu parameters
+// @param eta Cube containing current eta parameters
+// @param gamma Cube containing current gamma parameters
+// @param tilde_tau_phi vector containing current tilde_tau_phi parameters
+// @param xi Field of cubes containing all of the xi parameters
+// @param Z Matrix containing current Z parameters
+// @param sigma_sq double containing the sigma_sq variable
+// @param X matrix containing the covariates
+// @param chi Matrix containing chi values
+// @param iter int containing current mcmc sample
+// @param m_1 Vector acting as a placeholder for m in mean vector
+// @param M_1 Matrix acting as a placeholder for M in covariance
+// @param Phi Field of Cubes containing all mcmc samples of Phi
+inline void updatePhiMVCovariateAdj(const arma::mat& y_obs,
+                                    const arma::mat& nu,
+                                    const arma::cube& eta,
+                                    const arma::cube& gamma,
+                                    const arma::mat& tilde_tau_phi,
+                                    const arma::field<arma::cube>& xi,
+                                    const arma::mat& Z,
+                                    const arma::mat& chi,
+                                    const double& sigma_sq,
+                                    const arma::mat& X,
+                                    const int& iter,
+                                    const int& tot_mcmc_iters,
+                                    arma::vec& m_1,
+                                    arma::mat& M_1,
+                                    arma::field<arma::cube>& Phi){
+  m_1.zeros();
+  M_1.zeros();
+
+  for(int j =  0; j < Phi(iter,0).n_rows; j ++){
+    for(int m = 0; m < Phi(iter,0).n_slices; m++){
+      m_1.zeros();
+      M_1.zeros();
+      for(int i = 0; i < Z.n_rows; i++){
+        if(Z(i,j) != 0){
+          arma::vec ph = arma::zeros(y_obs.n_cols);
+          ph = y_obs.row(i).t() - Z(i,j) * (nu.row(j).t() +
+            (eta.slice(j) * X.row(i).t()));
+          M_1 = M_1 + Z(i,j) *  Z(i,j) * (chi(i,m) * chi(i,m) *
+            arma::eye(y_obs.n_cols, y_obs.n_cols));
+          for(int k = 0; k < nu.n_rows; k++){
+            for(int n = 0; n < Phi(iter,0).n_slices; n++){
+              if(k == j){
+                if(n != m){
+                  ph = ph - (Z(i,j) * chi(i,n) * Phi(iter,0).slice(n).row(k).t());
+                }
+                ph = ph - (Z(i,j) * chi(i,n) * (xi(iter,k).slice(n) * X.row(i).t()));
+              }else{
+                ph = ph - (Z(i,k) * chi(i,n) * (Phi(iter,0).slice(n).row(k).t() +
+                  (xi(iter,k).slice(n) * X.row(i).t())));
+              }
+            }
+            if(k != j){
+              ph = ph - Z(i,k) * (nu.row(k).t() +  (eta.slice(k) * X.row(i).t()));
+            }
+          }
+          m_1 = m_1 + Z(i,j) * chi(i,m) * ph;
+        }
+      }
+      m_1 = m_1 * (1 / sigma_sq);
+      M_1 = M_1 * (1 / sigma_sq);
+
+      //Add on diagonal component
+      for(int k = 0; k < M_1.n_rows; k++){
+        M_1(k,k) = M_1(k,k) + tilde_tau_phi(j,m) * gamma.slice(m)(j,k);
+      }
+      arma::inv(M_1, M_1);
+
+      //generate new sample
+      Phi(iter,0).slice(m).row(j) =  arma::mvnrnd(M_1 * m_1, M_1).t();
+    }
+  }
+  // Update next iteration
+  if(iter < (tot_mcmc_iters - 1)){
+    Phi(iter + 1,0) = Phi(iter,0);
+  }
+}
+
+// Updates the Phi parameters for the tempered multivariate covariate adjusted model
+//
+// @name UpdatePhiMVTemperedCovariateAd
+// @param beta_i Double containing the current temperature
+// @param y_obs Matrix containing observed vectors
+// @param nu Matrix containing current nu parameters
+// @param eta Cube containing current eta parameters
+// @param gamma Cube containing current gamma parameters
+// @param tilde_tau_phi vector containing current tilde_tau_phi parameters
+// @param xi Field of cubes containing all of the xi parameters
+// @param Z Matrix containing current Z parameters
+// @param sigma_sq double containing the sigma_sq variable
+// @param X matrix containing the covariates
+// @param chi Matrix containing chi values
+// @param iter int containing current mcmc sample
+// @param m_1 Vector acting as a placeholder for m in mean vector
+// @param M_1 Matrix acting as a placeholder for M in covariance
+// @param Phi Field of Cubes containing all mcmc samples of Phi
+inline void updatePhiTemperedMVCovariateAdj(const double& beta_i,
+                                            const arma::mat& y_obs,
+                                            const arma::mat& nu,
+                                            const arma::cube& eta,
+                                            const arma::cube& gamma,
+                                            const arma::mat& tilde_tau_phi,
+                                            const arma::field<arma::cube>& xi,
+                                            const arma::mat& Z,
+                                            const arma::mat& chi,
+                                            const double& sigma_sq,
+                                            const arma::mat& X,
+                                            const int& iter,
+                                            const int& tot_mcmc_iters,
+                                            arma::vec& m_1,
+                                            arma::mat& M_1,
+                                            arma::field<arma::cube>& Phi){
+  m_1.zeros();
+  M_1.zeros();
+
+  for(int j =  0; j < Phi(iter,0).n_rows; j ++){
+    for(int m = 0; m < Phi(iter,0).n_slices; m++){
+      m_1.zeros();
+      M_1.zeros();
+      for(int i = 0; i < Z.n_rows; i++){
+        if(Z(i,j) != 0){
+          arma::vec ph = arma::zeros(y_obs.n_cols);
+          ph = y_obs.row(i).t() - Z(i,j) * (nu.row(j).t() +
+            (eta.slice(j) * X.row(i).t()));
+          M_1 = M_1 + Z(i,j) *  Z(i,j) * (chi(i,m) * chi(i,m) *
+            arma::eye(y_obs.n_cols, y_obs.n_cols));
+          for(int k = 0; k < nu.n_rows; k++){
+            for(int n = 0; n < Phi(iter,0).n_slices; n++){
+              if(k == j){
+                if(n != m){
+                  ph = ph - (Z(i,j) * chi(i,n) * Phi(iter,0).slice(n).row(k).t());
+                }
+                ph = ph - (Z(i,j) * chi(i,n) * (xi(iter,k).slice(n) * X.row(i).t()));
+              }else{
+                ph = ph - (Z(i,k) * chi(i,n) * (Phi(iter,0).slice(n).row(k).t() +
+                  (xi(iter,k).slice(n) * X.row(i).t())));
+              }
+            }
+            if(k != j){
+              ph = ph - Z(i,k) * (nu.row(k).t() +  (eta.slice(k) * X.row(i).t()));
+            }
+          }
+          m_1 = m_1 + Z(i,j) * chi(i,m) * ph;
+        }
+      }
+      m_1 = m_1 * (beta_i / sigma_sq);
+      M_1 = M_1 * (beta_i / sigma_sq);
+
+      //Add on diagonal component
+      for(int k = 0; k < M_1.n_rows; k++){
+        M_1(k,k) = M_1(k,k) + tilde_tau_phi(j,m) * gamma.slice(m)(j,k);
+      }
+      arma::inv(M_1, M_1);
+
       //generate new sample
       Phi(iter,0).slice(m).row(j) =  arma::mvnrnd(M_1 * m_1, M_1).t();
     }
