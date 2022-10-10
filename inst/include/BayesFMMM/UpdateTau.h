a00b836a202ef6584b4e26aff692084475029de4
Nicholas Marco <ndmarco96@g.ucla.edu>
2022-10-10 13:15:08 -0700

    Added sampling from tau_eta

--- inst/include/BayesFMMM/UpdateTau.h
+++ inst/include/BayesFMMM/UpdateTau.h
@@ -10,10 +10,10 @@ namespace BayesFMMM{
 // @name updateTau
 // @param alpha Double containing hyperparameter
 // @param beta Double containing hyperparameter
-// @param nu Matrix contianing nu parameters
+// @param nu Matrix containing nu parameters
 // @param iter Int containing current MCMC iteration
 // @param tot_mcmc_iters Int containing total number of MCMC iterations
-// @param P Matrix contiaing tridiagonal P matrix
+// @param P Matrix containing tridiagonal P matrix
 // @param tau Matrix containing tau for all mcmc iterations
 inline void updateTau(const double& alpha,
                       const double& beta,
@@ -40,7 +40,7 @@ inline void updateTau(const double& alpha,
 // @name updateTauMV
 // @param alpha Double containing hyperparameter
 // @param beta Double containing hyperparameter
-// @param nu Matrix contianing nu parameters
+// @param nu Matrix containing nu parameters
 // @param iter Int containing current MCMC iteration
 // @param tot_mcmc_iters Int containing total number of MCMC iterations
 // @param tau Matrix containing tau for all mcmc iterations
@@ -59,6 +59,67 @@ inline void updateTauMV(const double& alpha,
   }
   if(iter < (tot_mcmc_iters - 1)){
     tau.row(iter + 1) = tau.row(iter);
+  }
+}
+
+// Updates the Tau_eta parameters for covariate adjusted model
+//
+// @name updateTauEta
+// @param alpha Double containing hyperparameter
+// @param beta Double containing hyperparameter
+// @param eta Cube containing eta parameters
+// @param iter Int containing current MCMC iteration
+// @param tot_mcmc_iters Int containing total number of MCMC iterations
+// @param P Matrix containing tridiagonal P matrix
+// @param tau_eta Cube containing tau_eta for all mcmc iterations
+inline void updateTauEta(const double& alpha,
+                         const double& beta,
+                         const arma::cube& eta,
+                         const int& iter,
+                         const int& tot_mcmc_iters,
+                         const arma::mat& P,
+                         arma::cube& tau_eta){
+  double a = 0;
+  double b = 0;
+
+  for(int j = 0; j < tau_eta.n_rows; j++){
+    for(int i = 0; i < tau_eta.n_cols; i++){
+      a = alpha + (eta.n_rows / 2);
+      b = beta + (0.5 * arma::dot(eta.slice(j).col(i), P * eta.slice(j).col(i)));
+      tau_eta(j, i, iter) =  R::rgamma(a, 1/b);
+    }
+  }
+  if(iter < (tot_mcmc_iters - 1)){
+    tau_eta.slice(iter + 1) = tau_eta.slice(iter);
+  }
+}
+
+// Updates the Tau parameters for the multivariate covariate adjusted model
+//
+// @name updateTauEtaMV
+// @param alpha Double containing hyperparameter
+// @param beta Double containing hyperparameter
+// @param eta Cube containing eta parameters
+// @param iter Int containing current MCMC iteration
+// @param tot_mcmc_iters Int containing total number of MCMC iterations
+// @param tau_eta Cube containing tau for all mcmc iterations
+inline void updateTauEtaMV(const double& alpha,
+                        const double& beta,
+                        const arma::cube& eta,
+                        const int& iter,
+                        const int& tot_mcmc_iters,
+                        arma::cube& tau_eta){
+  double a = 0;
+  double b = 0;
+  for(int j = 0; j < tau_eta.n_rows; j++){
+    for(int i = 0; i < tau_eta.n_cols; i++){
+      a = alpha + (eta.n_rows / 2);
+      b = beta + (0.5 * arma::dot(eta.slice(j).col(i), eta.slice(j).col(i)));
+      tau_eta(j, i, iter) =  1 / R::rgamma(a, 1/b);
+    }
+  }
+  if(iter < (tot_mcmc_iters - 1)){
+    tau_eta.slice(iter + 1) = tau_eta.slice(iter);
   }
 }
 
